b21efedd5a34e79454d07109eeae0b51b9ec59e5
nuzulul <nuzulz@gmail.com>
2025-05-30 16:24:04 +0700

    fix : fix cli

--- cli/wifidrop.js
+++ cli/wifidrop.js
@@ -1,3 +1,5 @@
+#!/usr/bin/env node
+
 import { spawn } from 'node:child_process';
 import { getAvailableBrowsers, launchBrowser } from 'detect-browsers';
 const browsers = await getAvailableBrowsers();
