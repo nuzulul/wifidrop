617f754ad201b699de10399b881f3d3cd112a1a5
nuzulul <nuzulz@gmail.com>
2025-06-06 16:01:58 +0700

    dist

---
+++ sw.js
@@ -0,0 +1,61 @@
+// This is the "Offline page" service worker
+
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
+
+const CACHE = "pwabuilder-page";
+
+// TODO: replace the following with the correct offline fallback page i.e.: const offlineFallbackPage = "offline.html";
+const offlineFallbackPage = "offline.html";
+
+self.addEventListener("message", (event) => {
+  if (event.data && event.data.type === "SKIP_WAITING") {
+    self.skipWaiting();
+  }
+});
+
+self.addEventListener('install', async (event) => {
+  event.waitUntil(
+    caches.open(CACHE)
+      .then((cache) => cache.add(offlineFallbackPage))
+  );
+});
+
+if (workbox.navigationPreload.isSupported()) {
+  workbox.navigationPreload.enable();
+}
+
+self.addEventListener('fetch', (event) => {
+
+  //console.log('event.request.url',event.request.url)
+  const url = new URL(event.request.url)
+  if (event.request.method === 'POST' && url.pathname === '/' && url.searchParams.has('share-target')) {
+		console.log('run share target start')
+        event.respondWith(Response.redirect('/?receiving-file-share=1'));
+        event.waitUntil(async function () {
+            const client = await self.clients.get(event.resultingClientId);
+            const data = await event.request.formData();
+            const files = data.getAll('files');
+            client.postMessage({ files });
+        }());
+		console.log('run share target finish')
+        return;
+   }else if (event.request.mode === 'navigate') {
+		event.respondWith((async () => {
+			  try {
+				const preloadResp = await event.preloadResponse;
+
+				if (preloadResp) {
+				  return preloadResp;
+				}
+
+				const networkResp = await fetch(event.request);
+				return networkResp;
+			  } catch (error) {
+
+				const cache = await caches.open(CACHE);
+				const cachedResp = await cache.match(offlineFallbackPage);
+				return cachedResp;
+			  }
+		})());
+  }
+});
