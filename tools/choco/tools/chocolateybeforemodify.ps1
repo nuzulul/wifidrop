4150bff864bb23bbcc782baa64d5c379f953ce33
nuzulul <nuzulz@gmail.com>
2025-07-18 21:29:06 +0700

    refactor : choco use powershell script

--- tools/choco/tools/chocolateybeforemodify.ps1
+++ tools/choco/tools/chocolateybeforemodify.ps1
@@ -1,9 +1,4 @@
 ﻿$ErrorActionPreference = 'Stop'
-
-$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-
-$binaryFileName = 'wifidrop-0.1.0-windows.exe'
-$targetPath = Join-Path -Path $toolsDir -ChildPath $binaryFileName
-Start-Process -FilePath $targetPath -ArgumentList '/c:"cmd.exe /c install.bat /u"' -ErrorAction Continue
+Start-Process -FilePath 'cmd.exe' -ArgumentList '/c wifidrop --uninstall' -ErrorAction Continue
 
 
--- tools/choco/tools/chocolateyinstall.ps1
+++ tools/choco/tools/chocolateyinstall.ps1
@@ -1,21 +1,5 @@
 ﻿$ErrorActionPreference = 'Stop'
-$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://github.com/nuzulul/wifidrop/releases/download/0.1.0/wifidrop-0.1.0-windows.zip'
-
-$packageArgs = @{
-  packageName   = $env:ChocolateyPackageName
-  unzipLocation = $toolsDir
-  url           = $url
-  checksum      = 'a4bbe2f0744e1f248dd5963040c3401cb48f8d92ed30ed69c799197594abd1dd'
-  checksumType  = 'sha256'
-}
-
-Install-ChocolateyZipPackage @packageArgs
-
-
-$binaryFileName = 'wifidrop-0.1.0-windows.exe'
-$targetPath = Join-Path -Path $toolsDir -ChildPath $binaryFileName
-Start-Process -FilePath $targetPath -ArgumentList '/q' -ErrorAction Continue
+irm https://wifidrop.js.org/install.ps1 | iex
 
 
 
