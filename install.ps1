8fb88e9a14256f77865d9e4b4d4ec51529e68fdb
nuzulul <nuzulz@gmail.com>
2026-04-09 11:11:03 +0700

    dist

---
+++ install.ps1
@@ -0,0 +1,10 @@
+# WIFIDrop
+Import-Module BitsTransfer;
+$requiredPath = Join-Path -Path $env:temp -ChildPath 'wifidrop';
+if (Test-Path -Path $requiredPath) {
+  Remove-Item -Path $requiredPath -Recurse -Force
+}
+New-Item -Path $requiredPath -ItemType Directory
+$targetPath = Join-Path -Path $env:temp -ChildPath 'wifidrop\install.bat';
+Start-BitsTransfer 'https://raw.githubusercontent.com/nuzulul/wifidrop/main/tools/util/install.bat' $targetPath
+Start-Process -FilePath 'cmd.exe' -ArgumentList "/c '@echo off & mode con cols=60 lines=20 & title WIFIDrop & $targetPath /s'" -ErrorAction Continue;
